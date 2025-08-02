eadb0f2a78711e2180fd3c41c029daf85b5b7ad6
MIYAKI Kojiro <miyaki-kojiro@core-k2.co.jp>
2025-08-02 18:25:45 +0900

    update version

--- xlsx.flow.js
+++ xlsx.flow.js
@@ -4,7 +4,7 @@
 /*global global, exports, module, require:false, process:false, Buffer:false, ArrayBuffer:false, DataView:false, Deno:false, Set:false, Float32Array:false */
 var XLSX = {};
 function make_xlsx_lib(XLSX){
-XLSX.version = '0.20.3.20250730';
+XLSX.version = '0.20.3.20250802';
 var current_codepage = 1200, current_ansi = 1252;
 /*:: declare var cptable:any; */
 /*global cptable:true, window */
@@ -26205,7 +26205,7 @@ function convert_content(wb, content, styles, opts, zip, setting) {
 				let c = decode_cell(n);
 				if (iRowMax <= c.r) iRowMax = c.r + 1;
 				if (iColMax < c.c) iColMax = c.c;
-				drawing2SVG(drawings[n], ass, dss, wb, sh, zip, styles);
+				drawing2SVG(drawings[n], ass, dss, wb, sh, zip, styles, fonts);
 			}
 		}
 		sh['!sn'] = sheet['style-name'];
@@ -26937,7 +26937,7 @@ function addDraw(drawings, draw, iCol, iRow, type) {
 	if (ret) drawings[cn] = ar;
 	return ret;
 }
-function getDrawStyle(name, st, styles, ass) {
+function getDrawStyle(name, st, styles, ass, fonts) {
 	let o = st && st[name];
 	if (typeof o === 'string') {
 		o = ass?.[o] || styles?.[o];
@@ -26946,11 +26946,18 @@ function getDrawStyle(name, st, styles, ass) {
 	if (styles && o?.['parent-style-name'] === 'Default') {
 		applyObject(obj, getDefaultStyle(styles, o.family), true);
 	}
+	let tp;
+	if (fonts && (tp = obj?.['text-properties'])) {
+		['font-name-asian', 'font-name'].forEach(n => {
+			let ff = fonts.find(f => f.name === tp[n]);
+			if (ff) tp[n] = ff['font-family'];
+		});
+	}
 	return obj;
 }
-function makeDrawStyle(draw, ass, dss, styles) {
+function makeDrawStyle(draw, ass, dss, styles, fonts) {
 	['style-name', 'text-style-name'].forEach((n, i) => {
-		let style = getDrawStyle(n, draw, styles, ass);
+		let style = getDrawStyle(n, draw, styles, ass, fonts);
 		if (!style) return;
 		delete draw[n];
 		if (Object.keys(style).length) {
@@ -26979,20 +26986,20 @@ function makeDrawImage(img, wb, zip, key) {
 	}
 	return null;
 }
-function drawing2SVG(draws, ass, dss, wb, ws, zip, styles) {
+function drawing2SVG(draws, ass, dss, wb, ws, zip, styles, fonts) {
 	if (!draws) return;
 	if (!Array.isArray(draws)) draws = [draws];
 	draws.forEach(draw => {
 		DRAW_SHAPES.forEach(n => {
 			const d = draw[n];
-			if (d) drawing2SVG(d, ass, dss, wb, ws, zip, styles);
+			if (d) drawing2SVG(d, ass, dss, wb, ws, zip, styles, fonts);
 		});
 		['width', 'height', 'x', 'y', 'end-x', 'end-y'].forEach(p => {
 			if (draw.hasOwnProperty(p)) {
 				draw[p] = Number(getPixelSize(draw[p]));
 			}
 		});
-		makeDrawStyle(draw, ass, dss, styles);
+		makeDrawStyle(draw, ass, dss, styles, fonts);
 		let p = draw.p;
 		if (p && !Array.isArray(p)) p = [p];
 		let img = draw.image;
@@ -27006,12 +27013,12 @@ function drawing2SVG(draws, ass, dss, wb, ws, zip, styles) {
 		}
 		if (p) {
 			p.forEach(p => {
-				makeDrawStyle(p, ass, dss);
+				makeDrawStyle(p, ass, dss, null, fonts);
 				let span = p.span;
 				if (span) {
 					if (!Array.isArray(span)) span = [span];
 					span.forEach(s => {
-						makeDrawStyle(s, ass, dss);
+						makeDrawStyle(s, ass, dss, null, fonts);
 					});
 				}
 			});
