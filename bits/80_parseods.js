6e3bea13d6aa61c505e1496a0f624f44ee5f9229
MIYAKI Kojiro <miyaki-kojiro@core-k2.co.jp>
2025-08-10 15:14:22 +0900

    ODS remember window fix position

--- bits/80_parseods.js
+++ bits/80_parseods.js
@@ -1121,8 +1121,19 @@ function convert_content(wb, content, styles, opts, zip, settings) {
 							if (!Array.isArray(v)) v = [v];
 							v.forEach(o => {
 								let sv = {};
-								const zoom = o.PageViewZoomValue || o.ZoomValue;
-								if (zoom) sv.zoomScale = zoom;
+								const zoom1 = o.ZoomValue;
+								const zoom2 = o.PageViewZoomValue;
+								if (zoom1 || zoom2) {
+									sv.zoomScale = zoom1 != 100 ? zoom1 : zoom2 || zoom1;
+								}
+								const xSplit = o.HorizontalSplitPosition;
+								const ySplit = o.VerticalSplitPosition;
+								if (xSplit || ySplit) {
+									sv.pane = {
+										xSplit: xSplit,
+										ySplit: ySplit,
+									};
+								}
 								if (!isEmpty(sv)) {
 									wb.Sheets[o.name].$sheetViews = {
 										sheetView: sv
