cd5d98d39cac4e25f9e8a98c0a0f7da02c37ae4b
MIYAKI Kojiro <miyaki-kojiro@core-k2.co.jp>
2025-10-13 16:33:56 +0900

    delete comment

--- xlsx.js
+++ xlsx.js
@@ -15489,7 +15489,6 @@ function parseDrawings(zip, dfile, ws, wb, styles, opts) {
 							const d = draws[m];
 							if (d?.graphicFrame?.graphic?.graphicData?.relIds?.dm === n) {
 								d.grpSp = o?.diagramDrawing?.spTree;
-								// Object.assign(d, o?.diagramDrawing?.spTree);
 								break;
 							}
 						}
