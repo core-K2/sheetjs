3193db7f9a61b5b1777893506bcb8d28c2cfc7b3
MIYAKI Kojiro <miyaki-kojiro@core-k2.co.jp>
2026-03-29 16:53:33 +0900

    update version

--- xlsx.flow.js
+++ xlsx.flow.js
@@ -4,7 +4,7 @@
 /*global global, exports, module, require:false, process:false, Buffer:false, ArrayBuffer:false, DataView:false, Deno:false, Set:false, Float32Array:false */
 var XLSX = {};
 function make_xlsx_lib(XLSX){
-XLSX.version = '0.20.3.20260125';
+XLSX.version = '0.20.3.20260329';
 var current_codepage = 1200, current_ansi = 1252;
 /*:: declare var cptable:any; */
 /*global cptable:true, window */
